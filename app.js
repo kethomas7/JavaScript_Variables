2d5583514664b25fc215b61fa807454baf79cc6f
Kaitlyn Thomas <kaitlynthomas2416@gmail.com>
2024-06-09 13:30:24 -0500

    completed all exercises

--- app.js
+++ app.js
@@ -5,6 +5,34 @@ console.log(
 
 // Exercise 1
 
-// Exercise 2
+let lastname;
+var age;
 
+// Exercise 2
+const firstname ="Kaitlyn";
+lastname = "Thomas";
+age = 27;
 // Exercise 3
+
+let language ="JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+
+console.log(firstname);
+
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = flase;
+let selectedSize = 'M';
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patrick Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
